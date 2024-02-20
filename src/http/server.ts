f98289f45252ab1dd7ec64953d0f78eb4cb35c16
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-20 12:39:36 -0300

    feat: persist polls in post/polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,19 +1,27 @@
 import fastify from "fastify";
 import { z } from "zod";
+import { PrismaClient } from "@prisma/client";
 
 const PORT = 3333;
 
 const app = fastify();
+const prisma = new PrismaClient();
 
-app.post("/polls", (request) => {
+app.post("/polls", async (request, reply) => {
     const createPollBody = z.object({
         title: z.string(),
     });
 
     const { title } = createPollBody.parse(request.body);
 
-    console.log("served_route: /polls");
-    return `título digitado: ${title}`;
+    const poll = await prisma.polls.create({
+        data: {
+            title,
+        },
+    });
+    console.log("route_served: post/polls");
+
+    return reply.status(201).send({ pollId: poll.id });
 });
 
 app.listen({ port: PORT }).then(() => {
