341856a63a3bfc56fa0931fcdbde3e9bd33da6f0
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-20 12:24:52 -0300

    feat: validate request.body

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,12 +1,19 @@
 import fastify from "fastify";
+import { z } from "zod";
 
 const PORT = 3333;
 
 const app = fastify();
 
 app.post("/polls", (request) => {
+    const createPollBody = z.object({
+        title: z.string(),
+    });
+
+    const { title } = createPollBody.parse(request.body);
+
     console.log("served_route: /polls");
-    return "/polls";
+    return `título digitado: ${title}`;
 });
 
 app.listen({ port: PORT }).then(() => {
