6545ca68c16e6f123c3c0be36fbddcc8a6753775
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-20 12:19:04 -0300

    feat: create route post/polls

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,14 @@
 import fastify from "fastify";
 
+const PORT = 3333;
+
 const app = fastify();
 
-app.listen({ port: 3333 }).then(() => {
-    console.log("server_status: running");
+app.post("/polls", (request) => {
+    console.log("served_route: /polls");
+    return "/polls";
+});
+
+app.listen({ port: PORT }).then(() => {
+    console.log(`server_status: running on http://localhost:${PORT}`);
 });
