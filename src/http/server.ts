393fedb3dd01c5ec151174efb011d72e28b9bc7d
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-26 14:56:58 -0300

    refactor: export create-poll separately

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,27 +1,13 @@
 import { prisma } from "../lib/prisma";
 import fastify from "fastify";
 import { z } from "zod";
+import { createPoll } from "./routes/create-poll";
 
 const PORT = 3333;
 
 const app = fastify();
 
-app.post("/polls", async (request, reply) => {
-    const createPollBody = z.object({
-        title: z.string(),
-    });
-
-    const { title } = createPollBody.parse(request.body);
-
-    const poll = await prisma.polls.create({
-        data: {
-            title,
-        },
-    });
-    console.log("route_served: post/polls");
-
-    return reply.status(201).send({ pollId: poll.id });
-});
+app.register(createPoll);
 
 app.listen({ port: PORT }).then(() => {
     console.log(`server_status: running on http://localhost:${PORT}`);
