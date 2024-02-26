7138c3eacb810dd093339c381c3d5a90d8ab6102
jairo-sousa <jairo.sousa.rufino@gmail.com>
2024-02-26 16:06:58 -0300

    feat: create PollsOption and Polls simultaneously

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -3,20 +3,30 @@ import { prisma } from "../../lib/prisma";
 import { FastifyInstance } from "fastify";
 
 export async function createPoll(app: FastifyInstance) {
-    app.post("/poll", async (request, reply) => {
+    app.post("/polls", async (request, reply) => {
         const createPollBody = z.object({
             title: z.string(),
+            options: z.array(z.string()),
         });
 
-        const { title } = createPollBody.parse(request.body);
+        const { title, options } = createPollBody.parse(request.body);
 
         const poll = await prisma.poll.create({
             data: {
                 title,
+                options: {
+                    createMany: {
+                        data: options.map((option) => {
+                            return {
+                                title: option,
+                            };
+                        }),
+                    },
+                },
             },
         });
-        console.log("route_served: post/poll");
 
+        console.log("route_served: post/polls");
         return reply.status(201).send({ pollId: poll.id });
     });
 }
